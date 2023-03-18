9802cb6611566119288040d620861e04298e4acd
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-03-18 23:11:55 +0000

    Feat(Commit.go): Added ParseCliArgs

---
+++ utilities/Commit.go
@@ -0,0 +1,16 @@
+package utilities
+
+import (
+// "fmt"
+)
+
+func Commit() {
+	ParseCliArgs()
+	/*
+		commitType := GetCommitType()
+		scope := GetScope()
+		message := GetCommitMessage()
+
+		fmt.Println(commitType, scope, message)
+	*/
+}
