28428e6029c1e285783b7789d46fe6cd7ac1334a
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-03-18 23:12:41 +0000

    Feat(ParseCliArgs.go): Made Own Arg Parser

---
+++ utilities/ParseCliArgs.go
@@ -0,0 +1,37 @@
+package utilities
+
+import (
+	"fmt"
+	"os"
+)
+
+func ParseCliArgs() {
+	for key, value := range ListArgs() {
+		fmt.Println("Key:", key)
+		fmt.Println("Value:", value)
+	}
+}
+
+func ListArgs() map[string]string {
+	resultMap := make(map[string]string)
+	ArgsArray := os.Args[1:]
+
+	for index := range ArgsArray {
+		firstCharacter := string([]rune(ArgsArray[index])[0:1])
+
+		if firstCharacter == "-" && len(ArgsArray[index]) <= 2 {
+			afterCharacter := string([]rune(ArgsArray[index])[1:2])
+
+			index = index + 1
+			for word := range ArgsArray[index:] {
+				firstCharacter := ArgsArray[index+word][0:1]
+				if firstCharacter == "-" {
+					break
+				}
+				// fmt.Println(ArgsArray[index:])
+				resultMap[afterCharacter] += ArgsArray[index:][word] + " "
+			}
+		}
+	}
+	return resultMap
+}
